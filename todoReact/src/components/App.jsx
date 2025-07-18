aadb60a3eb84bfd8549b85038bcb543f0a290e2e
SanjeevKumar3662 <sanjeevkumar3662work@gmail.com>
2025-07-18 17:15:11 +0530

    now items can be added to list

--- todoReact/src/components/App.jsx
+++ todoReact/src/components/App.jsx
@@ -1,26 +1,35 @@
-// import { useState } from 'react'
+import { useState } from 'react'
 // import reactLogo from './assets/react.svg'
 // import viteLogo from '/vite.svg'
 import { MakeLi } from "./Item";
 import "./App.css";
 
 function App() {
-  // const [val, setVal] = useState('Hello there Mr.');
-    
-  // }
+  const [val, setVal] = useState('My');
+  const [list, setList] = useState(["books - default"]);
+  
+  function updateName(e){
+    setVal(e.target.value);
+  }
+
+  function updateList(){
+    setList([...list,val]);
+  }
+  
   return (
     <>
       <div className="container">
-        <h1>My first react project</h1>
+        <h1>{val} first react project</h1>
         <div className="inputContainer">
           <input
+            onChange={updateName}
             className="input"
             type="text"
             placeholder="Enter your list Item"
           />
 
           {/* <input className="input-btn" type="button" value="Add Item" /> */}
-          <button className="input-btn">Add Item</button>
+          <button onClick={updateList} className="input-btn">Add Item</button>
         </div>
 
         <div className="filter">
@@ -28,14 +37,12 @@ function App() {
         </div>
 
         <ul className="list">
-          <MakeLi text={"first item"} />
-          <MakeLi text={"first item"} />
-          <MakeLi text={"first item"} />
-          <MakeLi text={"first item"} />
-          <MakeLi text={"first item"} />
+          <MakeLi key={"dummy"} text={"static item"} />
+         {list.map((ele,i)=>{
+          return <MakeLi key={i} text={ele}></MakeLi>})}
         </ul>
       </div>
-      init();
+      
     </>
   );
 }
