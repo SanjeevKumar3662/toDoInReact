6d09cb08ac37074443d759e4055da73a0d530ef8
SanjeevKumar3662 <sanjeevkumar3662work@gmail.com>
2025-07-18 19:03:45 +0530

    added some comments

--- todoReact/src/components/App.jsx
+++ todoReact/src/components/App.jsx
@@ -1,48 +1,52 @@
-import { useState } from 'react'
-// import reactLogo from './assets/react.svg'
-// import viteLogo from '/vite.svg'
-import { MakeLi } from "./Item";
+import { useState } from "react";
+import { Item } from "./Item";
 import "./App.css";
 
 function App() {
-  const [val, setVal] = useState('My');
-  const [list, setList] = useState(["books - default"]);
   
-  function updateName(e){
+  const [val, setVal] = useState("My"); // input value from 
+  const [list, setList] = useState(["books - default will an empty arr"]);//list that holds item value
+
+  function updateVal(e) {
     setVal(e.target.value);
   }
 
-  function updateList(){
-    setList([...list,val]);
+  function updateList(e) {
+    e.preventDefault();// stops add btn from submitting the form
+    setList([...list, val]);// add val to list
+    setVal("");//current val is not needed clear the val
   }
-  
+
   return (
     <>
       <div className="container">
-        <h1>{val} first react project</h1>
-        <div className="inputContainer">
+        <h1>{val} Sanjeev's first react project</h1>
+        <form className="inputContainer">
           <input
-            onChange={updateName}
+            onChange={updateVal}
             className="input"
             type="text"
             placeholder="Enter your list Item"
           />
 
-          {/* <input className="input-btn" type="button" value="Add Item" /> */}
-          <button onClick={updateList} className="input-btn">Add Item</button>
-        </div>
+          <button onClick={updateList} className="input-btn">
+            Add Item
+          </button>
+        </form>
 
         <div className="filter">
           <input className="input" type="text" placeholder="Search Item" />
         </div>
 
         <ul className="list">
-          <MakeLi key={"dummy"} text={"static item"} />
-         {list.map((ele,i)=>{
-          return <MakeLi key={i} text={ele}></MakeLi>})}
+          {/* here list the array that holds the item  
+            and for every element of list we will call the Item componenet
+          */}
+          {list.map((ele, i) => {
+            return <Item key={i} text={ele}></Item>;
+          })}
         </ul>
       </div>
-      
     </>
   );
 }
