98d11ef75f216b1d744d96dd84002920bda0ef71
SanjeevKumar3662 <sanjeevkumar3662work@gmail.com>
2025-07-19 12:34:43 +0530

    added searching functionality

--- todoReact/src/components/App.jsx
+++ todoReact/src/components/App.jsx
@@ -1,8 +1,9 @@
-import { useEffect, useState } from "react";
+import { useEffect, useState, useSyncExternalStore } from "react";
 import { Item } from "./Item";
 import "./App.css";
 
 /* TODO
+  -search
   -make it live on vercel 
 */
 
@@ -13,18 +14,33 @@ function App() {
     const stored = localStorage.getItem("list"); // gets the json from storage
     if (stored === undefined) return ["list is empty"]; // if no list, then make and empty one
 
-    console.log(stored," useState-> only once at start"); //logging list
+    console.log(stored, " useState-> only once at start"); //logging list
 
     return JSON.parse(stored); //parses json to an array and returns it
   });
 
+  const [isSearching,upadateSearching] = useState();
+  const [filterdList, updateFL] = useState();
+
   useEffect(() => {
     localStorage.setItem("list", JSON.stringify(list)); //stores updated list to storage
-    console.log(list,"in useEffect -> when list is modified");
+    console.log(list, "in useEffect -> when list is modified");
   }, [list]); // when an ele of arr is modified, it triggers the CB function
 
   function updateVal(e) {
     setVal(e.target.value);
+  }
+
+  function search(e) {
+    const target = e.target.value; // value form search field
+    target === ""?upadateSearching(false):upadateSearching(true);
+     
+    console.log(target);
+    const show = list.filter((ele) => ele.includes(target)); // filtered list
+    console.log(show);
+
+    // if()
+    updateFL(show);
   }
 
   function updateList(e) {
@@ -61,18 +77,31 @@ function App() {
         </form>
 
         <div className="filter">
-          <input className="input" type="text" placeholder="Search Item" />
+          <input
+            onChange={(e) => search(e)}
+            className="input"
+            type="text"
+            placeholder="Search Item"
+          />
         </div>
 
         <ul className="list">
           {/* here list the array that holds the item  
             and for every element of list we will call the Item componenet
           */}
-          {list.map((ele, i) => {
+          {
+
+            // if we are searching then show the filtered list else the real list
+          isSearching?(filterdList.map((ele, i) => {
             return (
               <Item key={i} text={ele} deleteE={() => deleteItem(ele)}></Item>
             );
-          })}
+          })):(list.map((ele, i) => {
+            return (
+              <Item key={i} text={ele} deleteE={() => deleteItem(ele)}></Item>
+            );
+          }))}
+          
         </ul>
       </div>
     </>
